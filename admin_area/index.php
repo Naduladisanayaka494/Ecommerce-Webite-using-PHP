2946dae93bd7dc596c46935b3227993fdc3039b7
nadula.fourdiv <nadula.fourdiv@gmail.com>
2023-07-20 11:16:13 +0530

    admin panel design

---
+++ admin_area/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../style.css">
+
+    <style>
+     .adminimage{
+    width:100px;
+    object-fit: contain;
+
+
+}
+ .footer{
+  position:absolute;
+  bottom:0;
+ }
+ 
+    </style>
+</head>
+<body>
+  <div class="container-fluid p-0">
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+      <div class="container-fluid">
+        <img src="../images/logo.png" alt="" class="logo2">
+        <nav class="navbar navbar-expand-lg">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a href="" class="nav-link">Welcome guest</a>
+        </li>
+      </ul>
+
+    </nav>
+      </div>
+    </nav>
+    <div calss="bg-light">
+      <h3 class="text-center p-2">Manage Details</h3>
+    </div>
+    <div class="row">
+      <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
+        <div class="p-5">
+          <a href="#"><img src="../images/treecoconut.jpg" alt="" class="adminimage"></a>
+          <p class="text-light text-center" >Admin Name</p>
+        </div>
+        <div class="button text-center">
+          <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Products</a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1">View products</a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1">View Brands</a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1">All Brands</a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1">All orders</a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1">All payments</a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1">List Users</a></button>
+          <button><a href="" class="nav-link text-light bg-info my-1">Logout</a></button>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="bg-info p-3 text-center footer">
+        <p>All right reserved D- Design by Nadula 2022</p>
+
+    </div>
+  
+
+  </div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    
+</body>
+</html>
