bf01471fed913d24fff1ec5647529d4181ebe034
nadula.fourdiv <nadula.fourdiv@gmail.com>
2023-08-06 17:29:07 +0530

    user registration_form

---
+++ users_area/user_registration.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>User-Rigistration</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+  <!-- Font Awesome CSS CDN (Font Awesome 5) -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    integrity="sha512-KT9MsB9k75XHk2vjAWPauD02Gk57qo0eGyFcaC91bBFK4AtxfLZ9b8eyqq8d9hA3nV/TFDt5fhCyBhwGuVc2A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <style>
+    /* Custom CSS to move search to the left */
+    .search-form {
+      display: flex;
+    }
+
+    .search-form .form-control {
+      margin-right: 5px;
+    }
+
+    /* Additional styles to adjust the height of the navbar */
+    .navbar {
+      min-height: 50px;
+    }
+
+    .navbar .navbar-brand {
+      margin-right: 20px;
+    }
+  </style>
+  <link rel="stylesheet" href="style.css">
+
+  <!-- Font Awesome JS CDN -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"
+    integrity="sha512-AWnAjMF96LlXzX6KRScV3F+grv7/6RXgeALbzT0H3iPn02vHNNO3VYKdEsmVT5ez0B7NYNjbJGRvPllfiYxRw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+<body>
+    <div class="container-fluid my-3">
+        <h2 class="text-center">New User Registration</h2>
+        <div class="row d-flex align-items-center justify-content-center">
+        <div class="col-lg-12 col-xl-6">
+            <form action="" method="post" enctypes="multipart/form-data">
+                <div class="form-outline">
+                    <label for="user_username" class="form-label">Username</label>
+                    <input type="text" id="user_username" class="form-control" placeholder="Enter your name" autocomplete="off" required="required" name="user_username"/>
+                </div>
+                <div class="form-outline mb-4">
+                    <label for="user_email" class="form-label">Email</label>
+                    <input type="email" id="user_email" class="form-control" placeholder="Enter your email" autocomplete="off" required="required" name="user_email"/>
+                </div>
+                <div class="form-outline mb-4">
+                    <label for="user_image" class="form-label">Image</label>
+                    <input type="file" id="user_image" class="form-control" required="required" name="user_image"/>
+                </div>
+                <div class="form-outline mb-4">
+                    <label for="user_password" class="form-label">Password</label>
+                    <input type="password" id="user_password" class="form-control" placeholder="Enter your Password" autocomplete="off" required="required" name="user_password"/>
+                </div>
+                <div class="form-outline mb-4">
+                    <label for="conf_user_password" class="form-label">Confirm Password</label>
+                    <input type="password" id="conf_user_password" class="form-control" placeholder="Confirm your Password" autocomplete="off" required="required" name="conf_user_password"/>
+                </div>
+                <div class="form-outline mb-4">
+                    <label for="user_address" class="form-label">Address</label>
+                    <input type="text" id="user_address"  class="form-control" placeholder="Enter your Address" autocomplete="off" required="required" name="user_address" />
+                </div>
+                <div class="form-outline mb-4">
+                    <label for="user_contact" class="form-label">Contact</label>
+                    <input type="text" id="user_address"  class="form-control" placeholder="Enter your Address" autocomplete="off" required="required" name="user_address" />
+                </div>
+                <div class="mt-4 pt-2">
+                    <input type="submit" value="Register" class="bg-info py-2 px-3 border-0"/>
+                    <p class="small fw-bold mt-2 pt-1 mb-0">Already have an account?<a href="user_login.php" class="text-dangergiy">Login</p></a> 
+
+                </div>
+
+            </form>
+
+
+        </div>
+        </div>
+    </div>
+    
+</body>
+</html>
