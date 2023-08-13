da88d9ded6ad247a769473450a619a0d5cae31f4
nadula.fourdiv <nadula.fourdiv@gmail.com>
2023-08-13 17:44:28 +0530

    changes for the things

--- users_area/user_login.php
+++ users_area/user_login.php
@@ -4,8 +4,72 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>User-Rigistration</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+  <!-- Font Awesome CSS CDN (Font Awesome 5) -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    integrity="sha512-KT9MsB9k75XHk2vjAWPauD02Gk57qo0eGyFcaC91bBFK4AtxfLZ9b8eyqq8d9hA3nV/TFDt5fhCyBhwGuVc2A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <style>
+    /* Custom CSS to move search to the left */
+    .search-form {
+      display: flex;
+    }
+
+    .search-form .form-control {
+      margin-right: 5px;
+    }
+
+    /* Additional styles to adjust the height of the navbar */
+    .navbar {
+      min-height: 50px;
+    }
+
+    .navbar .navbar-brand {
+      margin-right: 20px;
+    }
+  </style>
+  <link rel="stylesheet" href="style.css">
+
+  <!-- Font Awesome JS CDN -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"
+    integrity="sha512-AWnAjMF96LlXzX6KRScV3F+grv7/6RXgeALbzT0H3iPn02vHNNO3VYKdEsmVT5ez0B7NYNjbJGRvPllfiYxRw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 <body>
-    <h1>User login</h1>
+    <div class="container-fluid my-3">
+        <h2 class="text-center"> User Login</h2>
+        <div class="row d-flex align-items-center justify-content-center">
+        <div class="col-lg-12 col-xl-6">
+            <form action="" method="post" enctypes="multipart/form-data">
+                <div class="form-outline">
+                    <label for="user_username" class="form-label">Username</label>
+                    <input type="text" id="user_username" class="form-control" placeholder="Enter your name" autocomplete="off" required="required" name="user_username"/>
+                </div>
+                <div class="form-outline mb-4">
+                    <label for="user_password" class="form-label">Password</label>
+                    <input type="password" id="user_password" class="form-control" placeholder="Enter your Password" autocomplete="off" required="required" name="user_password"/>
+                </div>
+                <div class="mt-4 pt-2">
+                    <input type="submit" value="login" class="bg-info py-2 px-3 border-0" name="user_login"/>
+                    <p class="small fw-bold mt-2 pt-1 mb-0">Not Registerd yet?<a href="http://localhost//Ecommercewebsite/users_area/user_registration.php" class="text-dangergiy">Register</p></a> 
+
+                </div>
+
+            </form>
+
+
+        </div>
+        </div>
+    </div>
+    
 </body>
 </html>
--- users_area/user_registration.php
+++ users_area/user_registration.php
@@ -1,3 +1,10 @@
+<?php include('../includes/connect.php');
+ include('../functions/commonfunctions.php')
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -49,7 +56,8 @@
         <h2 class="text-center">New User Registration</h2>
         <div class="row d-flex align-items-center justify-content-center">
         <div class="col-lg-12 col-xl-6">
-            <form action="" method="post" enctypes="multipart/form-data">
+          <form action="" method="post" enctype="multipart/form-data">
+
                 <div class="form-outline">
                     <label for="user_username" class="form-label">Username</label>
                     <input type="text" id="user_username" class="form-control" placeholder="Enter your name" autocomplete="off" required="required" name="user_username"/>
@@ -76,10 +84,10 @@
                 </div>
                 <div class="form-outline mb-4">
                     <label for="user_contact" class="form-label">Contact</label>
-                    <input type="text" id="user_address"  class="form-control" placeholder="Enter your Address" autocomplete="off" required="required" name="user_address" />
+                    <input type="text" id="user_address"  class="form-control" placeholder="Enter your Address" autocomplete="off" required="required" name="user_contact" />
                 </div>
                 <div class="mt-4 pt-2">
-                    <input type="submit" value="Register" class="bg-info py-2 px-3 border-0"/>
+                    <input type="submit" value="Register" class="bg-info py-2 px-3 border-0" name="user_registration"/>
                     <p class="small fw-bold mt-2 pt-1 mb-0">Already have an account?<a href="user_login.php" class="text-dangergiy">Login</p></a> 
 
                 </div>
@@ -93,3 +101,29 @@
     
 </body>
 </html>
+
+<?php
+if(isset($_POST['user_registration'])){
+  $user_username=$_POST['user_username'];
+  $user_email=$_POST['user_email'];
+  $user_password=$_POST['user_password'];
+  $conf_user_password=$_POST['conf_user_password'];
+  $user_address=$_POST['user_address'];
+  $user_contact=$_POST['user_contact'];
+  $user_image1=$_FILES['user_image']['name'];
+  $user_image_tmp=$_FILES['user_image']['tmp_name'];
+  $user_ip= getIPAddress();
+  
+  move_uploaded_file($user_image_tmp,"./user_images/$user_image1");
+$insert_query="INSERT INTO `user_table` (username, user_email, user_password, user_image, user_ip, user_address, user_mobile) VALUES ('$user_username', '$user_email', '$user_password', '$user_image1', '$user_ip', '$user_address', '$user_contact')";
+  $sql_execute=mysqli_query($con,$insert_query);
+  if($sql_execute){
+    echo "<script>alert('Data inserted sucessfully')</script>";
+  }else{
+      die(mysqli_error($con));
+  }
+}
+
+
+
+?>
