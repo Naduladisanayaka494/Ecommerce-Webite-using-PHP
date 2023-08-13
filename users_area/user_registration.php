60e93352adee2e6bca6a83f5df26f74b37d83c02
nadula.fourdiv <nadula.fourdiv@gmail.com>
2023-08-13 18:24:20 +0530

    password hashing

--- users_area/user_registration.php
+++ users_area/user_registration.php
@@ -106,7 +106,8 @@
 if(isset($_POST['user_registration'])){
   $user_username=$_POST['user_username'];
   $user_email=$_POST['user_email'];
-  $user_password=$_POST['user_password'];
+$user_password = $_POST['user_password'];
+$hash_password = password_hash($user_password, PASSWORD_DEFAULT);
   $conf_user_password=$_POST['conf_user_password'];
   $user_address=$_POST['user_address'];
   $user_contact=$_POST['user_contact'];
@@ -124,7 +125,7 @@ if(isset($_POST['user_registration'])){
     echo "<script>alert('Passwords not match')</script>";
   }else{
       move_uploaded_file($user_image_tmp,"./user_images/$user_image1");
-$insert_query="INSERT INTO `user_table` (username, user_email, user_password, user_image, user_ip, user_address, user_mobile) VALUES ('$user_username', '$user_email', '$user_password', '$user_image1', '$user_ip', '$user_address', '$user_contact')";
+$insert_query="INSERT INTO `user_table` (username, user_email, user_password, user_image, user_ip, user_address, user_mobile) VALUES ('$user_username', '$user_email', '$hash_password', '$user_image1', '$user_ip', '$user_address', '$user_contact')";
   $sql_execute=mysqli_query($con,$insert_query);
   if($sql_execute){
     echo "<script>alert('Data inserted sucessfully')</script>";
